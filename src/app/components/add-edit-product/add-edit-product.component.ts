35afadc861d77b7c95b5096f9e8fe0679498c8fe
Rebeca Rodríguez <rebeca.rodriguez.guirao@gmail.com>
2024-03-26 02:50:51 +0100

    save product

--- src/app/components/add-edit-product/add-edit-product.component.ts
+++ src/app/components/add-edit-product/add-edit-product.component.ts
@@ -3,16 +3,21 @@ import { ActivatedRoute, RouterLink } from '@angular/router';
 import { FormBuilder, FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { Product } from '../../interfaces/product';
 import { Router } from '@angular/router';
+import { ProductService } from '../../services/product.service';
+import { ProgressBarComponent } from '../../shared/progress-bar/progress-bar.component';
+import { ToastrService } from 'ngx-toastr';
 
 
 @Component({
   selector: 'app-add-edit-product',
   standalone: true,
-  imports: [RouterLink, ReactiveFormsModule],
+  imports: [RouterLink, ReactiveFormsModule, ProgressBarComponent],
   templateUrl: './add-edit-product.component.html',
   styleUrl: './add-edit-product.component.scss'
 })
 export class AddEditProductComponent implements OnInit {
+  loading: boolean = false;
+
   get title(){
     return this.addForm.get('titulo') as FormControl;
   }
@@ -51,7 +56,7 @@ get age(){
 
 addForm: FormGroup;
 
-constructor(private fb : FormBuilder, private router: Router) { 
+constructor(private fb : FormBuilder, private router: Router, private _productService: ProductService, private toastr: ToastrService) { 
   this.addForm = this.fb.group({
     titulo : new FormControl('', [Validators.required, Validators.pattern(/^[a-zA-Z ]{3,}$/)]),
     fechaPublicacion : new FormControl('', [Validators.required, Validators.pattern(/^[1-2][0-9]{3}$/)]),
@@ -71,7 +76,7 @@ constructor(private fb : FormBuilder, private router: Router) {
   addProduct(){
     const product : Product = {
       // EAN: number,
-      id_juego: 0,
+      // id_juego: 0,
       titulo: this.addForm.value.titulo,
       fecha_publicacion: this.addForm.value.fechaPublicacion,
       // descripcion: string,
@@ -83,7 +88,13 @@ constructor(private fb : FormBuilder, private router: Router) {
       duracion_minutos: this.addForm.value.duracionMinutos,
       edad_min:  this.addForm.value.edadMin,
     }
-    console.table(product);
+
+    this.loading = true;
+    this._productService.saveProduct(product).subscribe(() => {
+    this.loading = false;
+    this.toastr.success(`El producto ${product.titulo} se añadido con éxito`, 'Producto añadido');
     this.router.navigate(['/home']);
+    })
+    
 }
 }
