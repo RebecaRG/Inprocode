0914f79f2f6c197047ce7492649c198d48684dda
Rebeca Rodríguez <rebeca.rodriguez.guirao@gmail.com>
2024-04-09 01:27:40 +0200

    remove comments from list-product

--- src/app/components/list-product/list-product.component.ts
+++ src/app/components/list-product/list-product.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Product } from '../../interfaces/product';
-import { RouterLink} from '@angular/router';
+import { RouterLink } from '@angular/router';
 import { ProductService } from '../../services/product.service';
 import { ProgressBarComponent } from '../../shared/progress-bar/progress-bar.component';
 import { ToastrService } from 'ngx-toastr';
@@ -14,60 +14,27 @@ import { ToastrService } from 'ngx-toastr';
   templateUrl: './list-product.component.html',
   styleUrl: './list-product.component.scss'
 })
-export class ListProductComponent implements OnInit{
+export class ListProductComponent implements OnInit {
 
-  listProducts: Product[] = [
-    // {
-    //   // id: 1,
-    //   // EAN: 8437020827591,
-    //   titulo: "Pelusas",
-    //   fechaPublicacion: 2022,
-    //   // descripcion: "A las pelusas les encanta ir acumulándose, pero cualquier corriente de aire puede hacer que desaparezcan en un momento...<br>Acumula pelusas para conseguir puntos, pero no cojas demasiadas o ¡puedes perder todas tus ganancias! <br> Roba cartas para conseguir puntos y decide si parar... o seguir robando otra carta. Pero, ¡cuidado!, te arriesgas a tener que descartar todas las cartas acumuladas...",
-    //   editorial: "Mercurio",
-    //   autoria: ["Reiner Knizia"],
-    //   ilustracion: ["Miguel Angel Galán"],
-    //   participantesMin: 2,
-    //   participantesMax: 6,
-    //   duracionMinutos: 20,
-    //   edadMin: 8,
-    //   // categoria: ["juegos de cartas"],
-    //   // url: "https://mercurio.com.es/pelusas.html",
-    // },
-    // {
-    //   // id: 2,
-    //   // EAN: 9788460659662,
-    //   titulo: "Virus!",
-    //   fechaPublicacion: 2015,
-    //   // descripcion: "Virus! es un juego de corte familiar en el que nuestro objetivo es conseguir aislar un cuerpo sano y erradicar el virus antes de que cualquier otra persona lo consiga. Este juego no entiende de ética y todo es válido para conseguir la victoria.",
-    //   editorial: "Tranjis",
-    //   autoria: ["Carlos López", "Domingo Cabrero", "Santi Santisteban"],
-    //   ilustracion: ["	David GJ", "Domingo Cabrero", "Santi Santisteban"],
-    //   participantesMin: 2,
-    //   participantesMax: 6,
-    //   duracionMinutos: 20,
-    //   edadMin: 8,
-    //   // categoria: ["juegos de cartas"],
-    //   // url: "https://tranjisgames.com/tienda/juego-de-mesa-virus/",
-    // }
-  ]
+  listProducts: Product[] = []
   loading: boolean = false;
 
-  constructor(private _productService: ProductService, private toastr: ToastrService){}
+  constructor(private _productService: ProductService, private toastr: ToastrService) { }
 
   ngOnInit(): void {
     this.getListProducts();
   }
 
-  getListProducts(){
+  getListProducts() {
     this.loading = true;
     this._productService.getListProducts().subscribe((data: Product[]) => {
       this.listProducts = data;
       this.loading = false;
     })
-    
+
   }
 
-  deleteProduct(id: number){
+  deleteProduct(id: number) {
     this.loading = true;
     this._productService.deleteProduct(id).subscribe(() => {
       this.getListProducts();
